89c7cf479313330231e9f9abf478f4102b8007c2
Rafael Borges <rafael.borges@luizalabs.com>
2021-11-13 17:46:35 -0300

    use global route prexi

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
   await app.listen(PORT);
 }
 
